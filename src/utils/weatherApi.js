fc6de61aa583c69ff6b6dbeabedb7ee00b6492ea
Cole Greenwald <colegreenwald@yahoo.com>
2024-11-25 22:58:41 -0800

    finalize for submit 1

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -1,6 +1,6 @@
 export const getWeather = ({ latitude, longitude }, APIkey) => {
   return fetch(
-    "https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}"
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
   ).then((res) => {
     if (res.ok) {
       return res.json();
@@ -15,7 +15,13 @@ export const filterWeatherData = (data) => {
   result.city = data.name;
   result.temp = { F: data.main.temp };
   result.type = getWeatherType(result.temp.F);
+  result.condition = data.weather[0].main.toLowerCase();
+  result.isDay = isDay(data.sys, Date.now());
   return result;
+};
+
+const isDay = ({ sunrise, sunset }, now) => {
+  return sunrise * 1000 < now && now < sunset * 1000;
 };
 
 const getWeatherType = (temperature) => {
